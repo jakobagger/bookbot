42c8bb1b214d9d99565ed75c299910b2c61de455
jakobagger <jakobagger@gmail.com>
2025-08-29 23:38:05 +0200

    count words

---
+++ main.py
@@ -0,0 +1,12 @@
+from stats import count_words
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+    
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document.")
+
+main()
