51290f0811d1e79a5c8050a0aeea2d0f71886e6c
jakobagger <jakobagger@gmail.com>
2025-08-31 23:27:56 +0200

    print report with sorted data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_chars, sort_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,6 +7,17 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
+    char_counts = count_chars(book_text)
+    sorted_data = sort_chars(char_counts)
+    print("""
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------""")
+    
     print(f"{num_words} words found in the document.")
+    print("--------- Character Count -------")
+    for item in sorted_data:
+        if (item["char"].isalpha()):
+            print(item["char"],":", item["num"])
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,24 @@ def count_words(text):
     words = text.split()
     return len(words)
     
+def count_chars(text):
+    word_counts = {}
+    for char in text:
+        char = char.lower()
+        if char in word_counts:
+            word_counts[char] += 1
+        else:
+            word_counts[char] = 1
+    
+    return word_counts
+
+def sort_on(items):
+    return items["num"]
+
+def sort_chars(my_dict):
+    char_dicts = []
+    for key, value in my_dict.items():
+        entry = {"char" : key, "num" : value}
+        char_dicts.append(entry)
+    char_dicts.sort(reverse=True, key=sort_on)
+    return char_dicts
