73c5baa566eb6a2e66ac5bf79ed660af0abeccf9
jakobagger <jakobagger@gmail.com>
2025-09-01 02:12:04 +0200

    finally got the printing working right

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from stats import count_words, count_chars, sort_chars
+    
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_text)
+    char_counts = count_chars(book_text)
+    sorted_data = sort_chars(char_counts)
+    print_report(book_path, num_words, sorted_data)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
-def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    num_words = count_words(book_text)
-    char_counts = count_chars(book_text)
-    sorted_data = sort_chars(char_counts)
-    print("""
-============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
------------ Word Count ----------""")
-    
-    print(f"{num_words} words found in the document.")
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    for item in sorted_data:
-        if (item["char"].isalpha()):
-            print(item["char"],":", item["num"])
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 main()
