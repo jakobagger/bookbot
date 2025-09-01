8d1955d63cbe62f5a681b32e6b2ae98df65ef863
jakobagger <jakobagger@gmail.com>
2025-09-01 11:06:20 +0200

    support for commandline args to point at different books

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from stats import count_words, count_chars, sort_chars
+import sys
     
 def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text("books/frankenstein.txt")
+    try:
+        book_path = sys.argv[1]
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     char_counts = count_chars(book_text)
     sorted_data = sort_chars(char_counts)
